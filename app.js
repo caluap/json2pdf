70ed0c94debe130d30b2639a7390e7c314211908
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2021-01-27 18:02:45 -0300

    processes the json, generates the pdf, sends it back

--- app.js
+++ app.js
@@ -8,31 +8,34 @@ var app = express();
 
 // https://stackabuse.com/handling-cors-with-node-js/
 app.use(cors());
-
 app.use(formidable());
+// app.use(express.static("./"));
 
 app.post("/post", (req, res) => {
   //req.fields contains non-file fields
   //req.files contains files
   console.log(`Received data from ${req.headers.origin}...`);
-  let data = req.fields.data;
+  let data = req.fields;
 
   // https://stackabuse.com/reading-and-writing-json-files-with-node-js/
-  fs.writeFile("./data.json", data, (err) => {
+  fs.writeFile("./data.json", JSON.stringify(data), (err) => {
     if (err) throw err;
     console.log("Saved JSON file...");
 
     // https://stackabuse.com/executing-shell-commands-with-node-js/#:~:text=Conclusion-,Node.,be%20available%20via%20event%20listeners.
-    exec("ls -l data.json", (error, stdout, stderr) => {
+    // let command = `java -jar /Users/calua/repositorios/processing.py-3017-macosx/processing-py.jar json2pdf.py`;
+    let command =
+      "processing-java --sketch=/Users/calua/repositorios/json2pdf --run";
+
+    exec(command, (error, stdout, stderr) => {
       if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+        // console.log(`error: ${error.message}`);
       }
       if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+        // console.log(`stderr: ${stderr}`);
       }
       console.log(`stdout: ${stdout}`);
+      res.sendFile("/Users/calua/repositorios/json2pdf/output.pdf");
     });
   });
 });
